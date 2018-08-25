da3c8b2f45f3b89e1a16930a02173914a6ff1223
Jeremy Song <jeremy.ysong@gmail.com>
2018-08-25 15:03:49 -0700

    Remove @Documented annotation

--- src/main/java/org/jeremy/ContainsInts.java
+++ src/main/java/org/jeremy/ContainsInts.java
@@ -2,7 +2,6 @@ package org.jeremy;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -14,7 +13,6 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Documented
 @Constraint(validatedBy = {ContainsIntValidator.class})
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
--- src/main/java/org/jeremy/ContainsStrings.java
+++ src/main/java/org/jeremy/ContainsStrings.java
@@ -14,7 +14,6 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Documented
 @Constraint(validatedBy = {ContainsStringsValidator.class})
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
